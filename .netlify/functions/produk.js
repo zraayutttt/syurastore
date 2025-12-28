40ec9a9cf56ea9d67e9fc438efe18af75bac5992
zraayutttt <72989100+zraayutttt@users.noreply.github.com>
2025-12-28 12:58:44 +0700

    Simplify produk function response

--- .netlify/functions/produk.js
+++ .netlify/functions/produk.js
@@ -1,9 +1,6 @@
-exports.handler = async (event) => {
+exports.handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "Netlify function produk aktif"
-    })
+    body: "FUNCTION PRODUK AKTIF"
   };
 };
